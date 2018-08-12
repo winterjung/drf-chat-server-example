af46c666ed6fce40ab128937ed116ce5a10a8f88
jungwinter <wintermy201@gmail.com>
2018-08-12 21:41:56 +0900

    Init basic models with relations

--- api/chat/models.py
+++ api/chat/models.py
@@ -1,3 +1,19 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Room(models.Model):
+    title = models.CharField(max_length=100, blank=True, default='')
+    participants = models.ManyToManyField(User)
+
+    def __str__(self):
+        return self.title
+
+
+class Message(models.Model):
+    sender = models.ForeignKey(User, on_delete=models.CASCADE)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    content = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.content
