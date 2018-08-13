4cf990752c26a77b6549250487391e7f8c8022cf
jungwinter <wintermy201@gmail.com>
2018-08-13 19:44:01 +0900

    Return only user's stuff in views

--- api/chat/views.py
+++ api/chat/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import User
+from django.http import Http404
 from rest_framework import generics, viewsets
 from rest_framework_extensions.mixins import NestedViewSetMixin
 
@@ -14,7 +15,19 @@ class RoomViewSet(NestedViewSetMixin, viewsets.ModelViewSet):
     queryset = models.Room.objects.all()
     serializer_class = serializers.RoomSerializer
 
+    def get_queryset(self):
+        user = self.request.user
+        return user.room_set.all()
+
 
 class MessageViewSet(NestedViewSetMixin, viewsets.ModelViewSet):
     queryset = models.Message.objects.all()
     serializer_class = serializers.MessageSerializer
+
+    def get_queryset(self):
+        parents_query_dict = self.get_parents_query_dict()
+        room_id = parents_query_dict.get('room')
+        user = self.request.user
+        if not user.room_set.filter(id=room_id):
+            raise Http404
+        return super().get_queryset()
