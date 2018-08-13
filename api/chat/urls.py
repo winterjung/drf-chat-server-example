0339f78f52e6ae56d03736eb8bdff3dcebe11e7e
jungwinter <wintermy201@gmail.com>
2018-08-13 13:07:21 +0900

    Add jwt authentication views

--- api/chat/urls.py
+++ api/chat/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from rest_framework_extensions.routers import NestedRouterMixin
+from rest_framework_simplejwt import views as jwt_views
 
 from api.chat import views
 
@@ -22,4 +23,6 @@ rooms_router.register(
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('auth/login', jwt_views.TokenObtainPairView.as_view()),
+    path('auth/refresh', jwt_views.TokenRefreshView.as_view()),
 ]
