88837e72b4be12161137c22df8b8b483b20737e7
jungwinter <wintermy201@gmail.com>
2018-08-14 19:27:30 +0900

    Delete messages after 60 seconds in background

--- api/chat/signals.py
+++ api/chat/signals.py
@@ -1,8 +1,10 @@
-from django.db.models.signals import post_save
+from django.conf import settings
+from django.db.models.signals import post_save, post_migrate
 from django.dispatch import receiver
+from django.utils import timezone
 
 from api.chat.models import Message
-from api.chat.tasks import push
+from api.chat.tasks import push, msg_remover
 
 
 @receiver(post_save, sender=Message)
@@ -11,3 +13,16 @@ def push_notification(sender, instance, **kwargs):
     rest = msg.room.participants.exclude(id=msg.sender.id).all()
     for person in rest:
         push(to=person.id, content=msg.content)
+
+
+@receiver(post_migrate)
+def start_tasks(sender, **kwargs):
+    lifetime = settings.MSG_LIFETIME
+    msg_remover(
+        lifetime,
+        repeat=lifetime / 10,
+        schedule=dict(
+            run_at=timezone.now(),
+            action=1,
+        ),
+    )
--- api/chat/tasks.py
+++ api/chat/tasks.py
@@ -1,5 +1,10 @@
+from datetime import timedelta
+
 import requests
 from background_task import background
+from django.utils import timezone
+
+from api.chat.models import Message
 
 
 @background
@@ -12,3 +17,9 @@ def push(to, content):
         requests.post('localhost:7000', json=data)
     except requests.exceptions.InvalidSchema:
         pass
+
+
+@background
+def msg_remover(lifetime):
+    deadline = timezone.now() - timedelta(seconds=lifetime)
+    Message.objects.filter(created_at__lt=deadline).delete()
